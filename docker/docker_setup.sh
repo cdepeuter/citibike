4c0c273d542fc41818fb10615bcb1d60a48ecb81
Yahweh Manchester <rohan@acxede.net>
2017-02-25 21:18:50 +0000

    Added docker setup

---
+++ docker/docker_setup.sh
@@ -0,0 +1,12 @@
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
+sudo apt-get update
+sudo apt-get install docker-engine=1.13.1-0~ubuntu-xenial
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
