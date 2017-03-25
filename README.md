3a49f6caa09385d4a14af1b120e0a41c943d3cc2
Conrad De Peuter <conrad.depeuter@gmail.com>
2017-03-25 14:34:14 -0400

    remove django, add flask

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## Citibike exploration
 
 Work for Columbia Computer Systems for Data Sci W4121
+
+To run Flask App, navigate to /flask-app
+
+`$ python3 app.py`
+
+http://localhost:5000/map?city=nyc
+http://localhost:5000/map?city=dc
---
+++ flask-app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request, render_template
+import sys
+import requests
+
+
+app = Flask(__name__)
+
+
+###########
+### APP ###
+###########
+@app.route('/')
+def index():
+    return "hi this is the index"
+
+
+@app.route("/map")
+def map():
+    city = request.args.get('city')
+
+    if city == "dc":
+        return render_template('dc_test.html')
+    elif city == "nyc":
+        return render_template('nyc_test.html')
+
+    return "specify a city"
+
+
+if __name__ == "__main__":
+    #app.run(debug=True) # for dev
+    app.run() # for prod
